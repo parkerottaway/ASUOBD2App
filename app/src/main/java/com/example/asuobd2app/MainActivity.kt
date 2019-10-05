a0b641214ec54291f4a177e781393d222333b355
Parker Ottaway <pottaway@asu.edu>
2019-10-04 20:54:11 -0700

    Initial commit.

---
+++ app/src/main/java/com/example/asuobd2app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.asuobd2app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Tested with Google Pixel 2 with Android 10.0.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
